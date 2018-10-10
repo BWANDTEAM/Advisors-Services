28cf6261fe7dd7d702152d34d55ac4a6c539008c
TyraEl13 <frederic.coste@yahoo.fr>
2018-10-10 20:02:59 +0200

    allegement du service

--- src/main/java/org/hackathon/packapp/containerbank/web/AdvisorController.java
+++ src/main/java/org/hackathon/packapp/containerbank/web/AdvisorController.java
@@ -1,18 +1,22 @@
 
 package org.hackathon.packapp.containerbank.web;
 
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.dao.DataAccessException;
+
+import java.util.Collection;
+
+import org.hackathon.packapp.containerbank.model.Advisor;
 import org.hackathon.packapp.containerbank.model.Advisors;
-import org.hackathon.packapp.containerbank.service.BankService;
-import org.json.simple.parser.JSONParser;
+import org.hackathon.packapp.containerbank.repository.AdvisorRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author Wavestone
@@ -20,65 +24,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Controller
 public class AdvisorController {
 
-    private final BankService bankService;
+	private AdvisorRepository advisorRepository;
 
 
     @Autowired
-    public AdvisorController(BankService bankService) {
-        this.bankService = bankService;
-    }
-
-//    @RequestMapping(value = { "/advisors.html"})
-//    public String showAdvisorList(Map<String, Object> model) {
-//        // Here we are returning an object of type 'Advisors' rather than a collection of Advisor objects
-//        // so it is simpler for Object-Xml mapping
-//        Advisors advisors = new Advisors();
-//        advisors.getAdvisorList().addAll(this.bankService.findAdvisors());
-//        model.put("advisors", advisors);
-//        return "advisors/advisorList";
-//    }
-    
-//    @RequestMapping("/showAdvisorList")
-//    public
-//    @ResponseBody String showAdvisorList(Map<String, Object> model) throws JsonProcessingException {
-//        // Here we are returning an object of type 'Advisors' rather than a collection of Advisor objects
-//        // so it is simpler for Object-Xml mapping
-//        Advisors advisors = new Advisors();
-//        advisors.getAdvisorList().addAll(this.bankService.findAdvisors());
-////        model.put("advisors", advisors);
-////        return "advisors/advisorList";
-////        return advisors;c
-//        
-//        ObjectMapper mapper = new ObjectMapper();
-//        return mapper.writeValueAsString(advisors);
-//    }
-
-//    @RequestMapping(value = { "/advisors.json", "/advisors.xml"})
-//    public
-//    @ResponseBody
-//    Advisors showResourcesAdvisorList() {
-//        // Here we are returning an object of type 'Advisors' rather than a collection of Advisor objects
-//        // so it is simpler for JSon/Object mapping
-//        Advisors advisors = new Advisors();
-//        advisors.getAdvisorList().addAll(this.bankService.findAdvisors());
-//        return advisors;
-//    }
-    
+    public AdvisorController(AdvisorRepository advisorRepository) {
+        this.advisorRepository = advisorRepository;
+    }    
     
     @RequestMapping("/advisors")
     public
     @ResponseBody
-    String showResourcesAdvisorList() throws JsonProcessingException {
+    Advisors showResourcesAdvisorList() throws JsonProcessingException {
         // Here we are returning an object of type 'Advisors' rather than a collection of Advisor objects
         // so it is simpler for JSon/Object mapping
         Advisors advisors = new Advisors();
-        advisors.getAdvisorList().addAll(this.bankService.findAdvisors());
-//        JSONParser test = new JSONParser();
-//        test.
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(advisors);
-//        return advisors;
+        advisors.getAdvisorList().addAll(this.findAdvisors());
+        return advisors;
     }
-
-
+    
+    @Transactional(readOnly = true)
+    @Cacheable(value = "advisors")
+    private Collection<Advisor> findAdvisors() throws DataAccessException {
+        return advisorRepository.findAll();
+    }
+    
 }
